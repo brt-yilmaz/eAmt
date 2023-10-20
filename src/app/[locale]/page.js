60be8d6637f1ed975893aecb038e32dcae64ba79
sokolov574 <sokolovloveoleg@gmail.com>
2023-10-20 10:36:30 +0200

    added Search bar

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,9 +1,12 @@
+import SearchPanel from "@/components/searchbar/SearchPanel";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
   return (
     <>
-      <main className="flex flex-1 flex-col items-center justify-between p-24 "></main>
+      <main className="flex flex-1 flex-col items-center justify-between p-24 ">
+        <SearchPanel />
+      </main>
     </>
   );
 }
