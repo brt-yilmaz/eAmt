8b66b8bbc2168ede0c1a6c3d9fe6e3f7d5bd3678
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-10-31 13:51:10 +0100

    Backend-Feat: Implement sending email to verification

---
+++ src/helpers/mailer.js
@@ -0,0 +1,55 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcrypt from "bcryptjs";
+
+export const sendEmail = async (email, emailType, userId) => {
+  try {
+    const hashedToken = await bcrypt.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "poststelle@eamt.bund.de",
+      to: email,
+      subject:
+        emailType === "VERIFY"
+          ? "Activate your account"
+          : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+          or copy and paste the link below in your browser. <br> ${
+            process.env.DOMAIN
+          }/verifyemail?token=${hashedToken}
+          </p>`,
+    };
+
+    const mailResponse = await transport.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
