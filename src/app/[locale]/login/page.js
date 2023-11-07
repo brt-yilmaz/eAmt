ace5f4f967c2fcf7b012ff9bf310fc25f74cb0b4
sokolov574 <sokolovloveoleg@gmail.com>
2023-11-07 09:34:13 +0100

    added login and signup pages

---
+++ src/app/[locale]/login/page.js
@@ -0,0 +1,11 @@
+import LanguageSwitcher from '@/components/header/LanguageSwitcher'
+import Login from '@/components/login/Login'
+
+export default function Home() {
+  return (
+    <div>
+      <LanguageSwitcher />
+      <Login />
+    </div>
+  )
+}
