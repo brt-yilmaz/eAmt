ce6eb5af2054d1c53c1ac9d8b99bbee1f05c06a6
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-11-07 23:08:27 +0100

    Backend-Fix: Implement user lockout model and improve safety of login api

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -1,5 +1,6 @@
 import { connectDB } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
+import UserLockout from "@/models/userLockoutModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -12,7 +13,23 @@ export async function POST(req) {
     const reqBody = await req.json();
     const { email, password } = reqBody;
 
-    // check if user exists
+    // Check if user is locked out
+    const userLockout = await UserLockout.findOne({ email });
+
+    if (userLockout && userLockout.lockedUntil > Date.now()) {
+      return NextResponse.json(
+        {
+          error: "Your account has been locked. Please try again later.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    console.log("after first");
+
+    // Check if user exists
     const user = await User.findOne({ email });
     if (!user) {
       return NextResponse.json(
@@ -25,10 +42,60 @@ export async function POST(req) {
       );
     }
 
-    // check if password is correct
+    if (!user.isVerified) {
+      return NextResponse.json(
+        {
+          error: "User not verified, please verify your account",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // Check if password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
 
     if (!validPassword) {
+      // Record a failed password attempt
+
+      if (!userLockout) {
+        const newUserLockout = await UserLockout.create({
+          email,
+        });
+
+        newUserLockout.failedLoginAttempts += 1;
+        await newUserLockout.save();
+
+        return NextResponse.json(
+          {
+            error: "User or password incorrect",
+          },
+          {
+            status: 400,
+          }
+        );
+      }
+
+      userLockout.failedLoginAttempts += 1;
+
+      if (userLockout.failedLoginAttempts >= 3) {
+        userLockout.lockedUntil = Date.now() + 60000; // Lock the account for 1 minute
+        await userLockout.save();
+
+        return NextResponse.json(
+          {
+            error:
+              "Your account has been locked for 1 minute. Please try again later.",
+          },
+          {
+            status: 400,
+          }
+        );
+      } else {
+        await userLockout.save();
+      }
+
       return NextResponse.json(
         {
           error: "User or password incorrect",
@@ -39,41 +106,51 @@ export async function POST(req) {
       );
     }
 
-    // create token data
+    if (userLockout && userLockout.lockedUntil > Date.now()) {
+      return NextResponse.json(
+        {
+          error:
+            "Your account has been locked for 1 minute. Please try again later.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // Delete user lockout
+    await UserLockout.deleteOne({ userId: user._id });
+
     const tokenData = {
       id: user._id,
       name: user.name,
       email: user.email,
     };
 
-    // create token
     const passwordToken = jwt.sign(tokenData, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
 
-    // set token to cookie
     cookies().set({
       name: "passwordToken",
       value: passwordToken,
       httpOnly: true,
       expires: new Date(Date.now() + 3600000 * 24 * 7),
       secure: true,
+      sameSite: "strict",
     });
 
-    const res = NextResponse.json({
+    return NextResponse.json({
       success: true,
       message: "User logged in successfully",
     });
-
-    // set token to cookie
-    res.cookies.set("token", token, {
-      httpOnly: true,
-    });
-
-    return res;
   } catch (error) {
-    return NextResponse.json({
-      error: error.message,
-    });
+    return NextResponse.json(
+      {
+        error:
+          "An error occurred while processing your request. Please send an email to eamt3306@gmail.com",
+      },
+      { status: 500 }
+    );
   }
 }
