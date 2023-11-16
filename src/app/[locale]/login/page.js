c42bbb10e27d15d8ffe8d8ea4d7feaab98696c4c
sokolov574 <sokolovloveoleg@gmail.com>
2023-11-16 09:19:34 +0100

    added navbar for identity forms

--- src/app/[locale]/login/page.js
+++ src/app/[locale]/login/page.js
@@ -1,11 +1,22 @@
-import LanguageSwitcher from '@/components/header/LanguageSwitcher'
-import Login from '@/components/login/Login'
-
-export default function Home() {
+import LanguageSwitcher from "@/components/header/LanguageSwitcher";
+import ModeSwitcher from "@/components/header/ModeSwitcher";
+import NavBar from "@/components/header/NavBar";
+import LoginPage from "@/components/login/Login";
+function Login() {
   return (
-    <div>
-      <LanguageSwitcher />
-      <Login />
-    </div>
-  )
+    <>
+      <header
+        className={
+          "flex items-center justify-between px-10  py-[min(3vh,2rem)] sm:px-[min(3vh,2rem)]"
+        }
+      >
+        <NavBar>
+          <ModeSwitcher />
+          <LanguageSwitcher />
+        </NavBar>
+      </header>
+      <LoginPage />
+    </>
+  );
 }
+export default Login;
--- src/app/[locale]/signup/page.js
+++ src/app/[locale]/signup/page.js
@@ -20,13 +20,3 @@ function SignUp() {
   );
 }
 export default SignUp;
-// import LanguageSwitcher from '@/components/header/LanguageSwitcher'
-// import SignupPage from '@/components/signup/SignUp'
-// export default function SignUp() {
-//   return (
-//     <div>
-//       <LanguageSwitcher />
-//       <SignupPage />
-//     </div>
-//   )
-// }
